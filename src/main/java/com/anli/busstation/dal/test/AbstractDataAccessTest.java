69e19e16e503f69f8e12756c0855a8b76a53fb90
lyashenkoav92@gmail.com <andreiliashenko@users.noreply.github.com>
2015-04-04 13:01:19 +0000

    Data Access integration tests (basic and Ejb2) are refactored: DAL-module access is wrapped up to one class, possibility of cache reset added

--- src/main/java/com/anli/busstation/dal/test/AbstractDataAccessTest.java
+++ src/main/java/com/anli/busstation/dal/test/AbstractDataAccessTest.java
@@ -1,11 +1,36 @@
 package com.anli.busstation.dal.test;
 
+import com.anli.busstation.dal.interfaces.entities.BSEntity;
 import com.anli.busstation.dal.interfaces.factories.ProviderFactory;
 import com.anli.integrationtesting.execution.TestExecutor;
+import java.math.BigInteger;
 
 public abstract class AbstractDataAccessTest implements TestExecutor {
 
-    protected abstract FixtureCreator getFixtureCreator();
+    protected ModuleAccessor moduleAccessor;
 
-    protected abstract ProviderFactory getFactory();
+    protected FixtureCreator getFixtureCreator() {
+        return getModuleAccessor().getFixtureCreator();
+    }
+
+    protected ProviderFactory getFactory() {
+        return getModuleAccessor().getProviderFactory();
+    }
+
+    protected void resetCaches() {
+        getModuleAccessor().resetCaches();
+    }
+
+    protected void setEntityId(BSEntity entity, BigInteger id) {
+        getModuleAccessor().setEntityId(entity, id);
+    }
+
+    protected ModuleAccessor getModuleAccessor() {
+        if (moduleAccessor == null) {
+            moduleAccessor = createModuleAccessor();
+        }
+        return moduleAccessor;
+    }
+
+    protected abstract ModuleAccessor createModuleAccessor();
 }
